0c7ade964822c501f08e9f45af2979a575819824
k1mericz <ericjinzj@163.com>
2017-04-01 23:55:53 +0800

    Create items.py

---
+++ items.py
@@ -0,0 +1,20 @@
+# news_spider
+NIST's second homework
+
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NewsItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
+    body = scrapy.Field()
+    pass
